50e19c822e1e77f709c97a5eaa65d83cf46963b3
sandalu umayanga <sandaluumayangaofficial@gmail.com>
2024-12-08 16:35:36 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <nav className="navbar">
+        <h1>🌱 Navigation Bar</h1>
+        <ul>
+          <li>🔥 Content</li>
+          <ul>
+            <li>🔥 Introduction</li>
+            <ul>
+              <li>🕹 Topic Introduction</li>
+              <li>🕹 Research Problem</li>
+              <li>🕹 Objectives</li>
+            </ul>
+            <li>🔥 Literature Review</li>
+            <li>🔥 Methodology</li>
+            <ul>
+              <li>🕹 Research Approach</li>
+              <li>🕹 Research Philosophy</li>
+              <li>🕹 Study Area</li>
+              <li>🕹 Conceptual Framework</li>
+              <li>🕹 Operational Definition</li>
+              <li>🕹 Hypothesis</li>
+              <li>🕹 Population, Sample, Sampling</li>
+              <li>🕹 Data Collection Method</li>
+            </ul>
+            <li>🔥 Data Analysis</li>
+            <li>🔥 Create the Web Maps</li>
+            <li>🔥 Dashboard</li>
+            <li>🔥 Conclusion and Recommendations</li>
+          </ul>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default App;
